146aa693f580d46266dc87d33f70c338f95a04f4
IAserna <asernacamarillo@gmail.com>
2024-02-14 13:53:46 -0600

    interface productos

---
+++ src/app/servicios/productos.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../interfaces/producto';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  private baseUrl = "http://localhost:3000";
+
+  constructor(private http:HttpClient) { }
+
+  getProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(`${this.baseUrl}}`)
+  }
+}
+
